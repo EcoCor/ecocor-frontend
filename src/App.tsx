dracor-org/poecor-frontend

Add padding for page contents

src/App.tsx
@@ -9,12 +9,14 @@ function App() {
   return (
     <BrowserRouter>
       <Topnav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/corpora" element={<Corpora />} />
-        <Route path="/corpora/:id" element={<Corpus />} />
-        <Route path="/corpora/:corpusId/:poemId" element={<Poem />} />
-      </Routes>
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/corpora" element={<Corpora />} />
+          <Route path="/corpora/:id" element={<Corpus />} />
+          <Route path="/corpora/:corpusId/:poemId" element={<Poem />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }

EcoCor/ecocor-frontend

src/App.tsx
@@ -9,12 +9,14 @@ function App() {
   return (
     <BrowserRouter>
       <Topnav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/corpora" element={<Corpora />} />
-        <Route path="/corpora/:id" element={<Corpus />} />
-        <Route path="/corpora/:corpusId/:workId" element={<Work />} />
-      </Routes>
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/corpora" element={<Corpora />} />
+          <Route path="/corpora/:id" element={<Corpus />} />
+          <Route path="/corpora/:corpusId/:workId" element={<Work />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }