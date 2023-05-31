dracor-org/poecor-frontend

Connect components to (mock) API

src/Corpus.tsx
@@ -1,16 +1,53 @@
+import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { getCorpus } from './api';
+import { CorpusData } from './types';
 
 export default function Corpus() {
   const { id } = useParams<{ id: string }>();
+  const [corpus, setCorpus] = useState<CorpusData>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    (async function () {
+      setLoading(true);
+      try {
+        const resp = await getCorpus(id!);
+        if (isMounted) {
+          setCorpus(resp.data);
+        }
+      } catch (error) {
+        alert('Cannot load corpus');
+      }
+      if (isMounted) {
+        setLoading(false);
+      }
+    })();
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
   return (
     <div>
       <Helmet>
-        <title>Corpus: {id}</title>
+        <title>Corpus: {corpus?.title || 'loading...'}</title>
       </Helmet>
-      <section>
-        <h1>{id}</h1>
-      </section>
+      {loading && <p>loading...</p>}
+      {corpus && (
+        <section>
+          <h1>{corpus.title}</h1>
+          <ol>
+            {corpus.poems.map((poem) => (
+              <li key={poem.name}>
+                <Link to={`/corpora/${id}/${poem.name}`}>{poem.title}</Link>
+              </li>
+            ))}
+          </ol>
+        </section>
+      )}
     </div>
   );
 }

EcoCor/ecocor-frontend

src/Corpus.tsx
@@ -1,16 +1,53 @@
+import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { getCorpus } from './api';
+import { CorpusData } from './types';
 
 export default function Corpus() {
   const { id } = useParams<{ id: string }>();
+  const [corpus, setCorpus] = useState<CorpusData>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    (async function () {
+      setLoading(true);
+      try {
+        const resp = await getCorpus(id!);
+        if (isMounted) {
+          setCorpus(resp.data);
+        }
+      } catch (error) {
+        alert('Cannot load corpus');
+      }
+      if (isMounted) {
+        setLoading(false);
+      }
+    })();
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
   return (
     <div>
       <Helmet>
-        <title>Corpus: {id}</title>
+        <title>Corpus: {corpus?.title || 'loading...'}</title>
       </Helmet>
-      <section>
-        <h1>{id}</h1>
-      </section>
+      {loading && <p>loading...</p>}
+      {corpus && (
+        <section>
+          <h1>{corpus.title}</h1>
+          <ol>
+            {corpus.works.map((work) => (
+              <li key={work.name}>
+                <Link to={`/corpora/${id}/${work.name}`}>{work.title}</Link>
+              </li>
+            ))}
+          </ol>
+        </section>
+      )}
     </div>
   );
 }