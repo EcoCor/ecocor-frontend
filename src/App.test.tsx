dracor-org/poecor-frontend

Set up basic routing structure

src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders welcome message', () => {
+  render(
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  );
+  const elem = screen.getByText(/Welcome to PoeCor/i);
+  expect(elem).toBeInTheDocument();
 });

src/Home.tsx
@@ -1,0 +1,14 @@
+import { Helmet } from 'react-helmet-async';
+
+export default function Home() {
+  return (
+    <div>
+      <Helmet>
+        <title>PoeCor</title>
+      </Helmet>
+      <section>
+        <h1>Welcome to PoeCor</h1>
+      </section>
+    </div>
+  );
+}

src/Topnav.tsx
@@ -1,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+export default function Topnav() {
+  return (
+    <nav>
+      <h1>
+        <Link to="/">PoeCor</Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/corpora">Corpora</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}

EcoCor/ecocor-frontend

src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders welcome message', () => {
+  render(
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  );
+  const elem = screen.getByText(/Welcome to EcoCor/i);
+  expect(elem).toBeInTheDocument();
 });

src/Home.tsx
@@ -1,0 +1,14 @@
+import { Helmet } from 'react-helmet-async';
+
+export default function Home() {
+  return (
+    <div>
+      <Helmet>
+        <title>EcoCor</title>
+      </Helmet>
+      <section>
+        <h1>Welcome to EcoCor</h1>
+      </section>
+    </div>
+  );
+}

src/Topnav.tsx
@@ -1,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+export default function Topnav() {
+  return (
+    <nav>
+      <h1>
+        <Link to="/">EcoCor</Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/corpora">Corpora</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}