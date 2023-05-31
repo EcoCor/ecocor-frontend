dracor-org/poecor-frontend

Add basic TopNav styles
copied from einakter

src/Topnav.tsx
@@ -1,16 +1,24 @@
 import { Link } from 'react-router-dom';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Topnav() {
   return (
-    <nav>
+    <nav className="flex items-center justify-between flex-wrap p-4 bg-primary text-white font-medium">
       <h1>
-        <Link to="/">PoeCor</Link>
+        <Link to="/" className="text-white hover:text-white font-light">
+          Poetry Corpora
+        </Link>
       </h1>
-      <ul>
-        <li>
-          <Link to="/corpora">Corpora</Link>
-        </li>
-      </ul>
+      <div>
+        <a
+          href="https://github.com/dracor-org/poecor-frontend"
+          title="Einakter Github"
+          className="text-white"
+        >
+          <FontAwesomeIcon icon={faGithub} size="2xl" />
+        </a>
+      </div>
     </nav>
   );
 }

EcoCor/ecocor-frontend

src/Topnav.tsx
@@ -1,16 +1,24 @@
 import { Link } from 'react-router-dom';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Topnav() {
   return (
-    <nav>
+    <nav className="flex items-center justify-between flex-wrap p-4 bg-primary text-white font-medium">
       <h1>
-        <Link to="/">EcoCor</Link>
+        <Link to="/" className="text-white hover:text-white font-light">
+          EcoCor
+        </Link>
       </h1>
-      <ul>
-        <li>
-          <Link to="/corpora">Corpora</Link>
-        </li>
-      </ul>
+      <div>
+        <a
+          href="https://github.com/dh-network/ecocor-api"
+          title="EcoCor Github"
+          className="text-white"
+        >
+          <FontAwesomeIcon icon={faGithub} size="2xl" />
+        </a>
+      </div>
     </nav>
   );
 }