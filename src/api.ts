dracor-org/poecor-frontend

Implement API functions
To fix jest we need to adjust the yarn test script.
See https://stackoverflow.com/questions/51994111/jest-encountered-an-unexpected-token

src/api.ts
@@ -1,0 +1,49 @@
+import axios, {
+  AxiosRequestConfig,
+  Method,
+  CancelTokenSource,
+  AxiosResponse,
+} from 'axios';
+import { CorpusData, CorpusListEntry, Poem } from './types';
+
+const apiUrl = process.env.REACT_APP_POECOR_API;
+
+const defaultOpts: AxiosRequestConfig = {
+  withCredentials: true,
+};
+
+async function fetchData<T>(
+  url: string,
+  options?: {
+    data?: object;
+    method?: Method;
+    cancelTokenSource?: CancelTokenSource;
+  }
+): Promise<AxiosResponse<T>> {
+  return await axios(url, {
+    ...defaultOpts,
+    data: options?.data,
+    method: options?.method,
+    cancelToken: options?.cancelTokenSource?.token,
+  });
+}
+
+export async function getCorpora(): Promise<AxiosResponse<CorpusListEntry[]>> {
+  const url = `${apiUrl}/corpora`;
+  return await fetchData<CorpusListEntry[]>(url);
+}
+
+export async function getCorpus(
+  name: string
+): Promise<AxiosResponse<CorpusData>> {
+  const url = `${apiUrl}/corpora/${name}`;
+  return await fetchData<CorpusData>(url);
+}
+
+export async function getPoem(
+  corpusName: string,
+  poemName: string
+): Promise<AxiosResponse<Poem>> {
+  const url = `${apiUrl}/corpora/${corpusName}/poem/${poemName}`;
+  return await fetchData<Poem>(url);
+}

EcoCor/ecocor-frontend

src/api.ts
@@ -1,0 +1,49 @@
+import axios, {
+  AxiosRequestConfig,
+  Method,
+  CancelTokenSource,
+  AxiosResponse,
+} from 'axios';
+import { CorpusData, CorpusListEntry, Work } from './types';
+
+const apiUrl = process.env.REACT_APP_ECOCOR_API;
+
+const defaultOpts: AxiosRequestConfig = {
+  withCredentials: true,
+};
+
+async function fetchData<T>(
+  url: string,
+  options?: {
+    data?: object;
+    method?: Method;
+    cancelTokenSource?: CancelTokenSource;
+  }
+): Promise<AxiosResponse<T>> {
+  return await axios(url, {
+    ...defaultOpts,
+    data: options?.data,
+    method: options?.method,
+    cancelToken: options?.cancelTokenSource?.token,
+  });
+}
+
+export async function getCorpora(): Promise<AxiosResponse<CorpusListEntry[]>> {
+  const url = `${apiUrl}/corpora`;
+  return await fetchData<CorpusListEntry[]>(url);
+}
+
+export async function getCorpus(
+  name: string
+): Promise<AxiosResponse<CorpusData>> {
+  const url = `${apiUrl}/corpora/${name}`;
+  return await fetchData<CorpusData>(url);
+}
+
+export async function getWork(
+  corpusName: string,
+  workName: string
+): Promise<AxiosResponse<Work>> {
+  const url = `${apiUrl}/corpora/${corpusName}/works/${workName}`;
+  return await fetchData<Work>(url);
+}