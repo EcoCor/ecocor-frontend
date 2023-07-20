c36d51c5b363eaa6d10ed8323c78d1af17f64ac8
Carsten Milling <cmil@hashtable.de>
2023-07-20 11:48:32 +0200

    Add @dracor.org/react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './node_modules/@dracor.org/react/**/*.js',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {
       animation: {
@@ -13,18 +16,7 @@ module.exports = {
           },
         },
       },
-      colors: {
-        primary: '#1f2448',
-        secondary: {
-          100: '#aef',
-          200: '#08f',
-        },
-        neutral: {
-          100: '#ebf0f7',
-          200: '#1f244809',
-        },
-      },
     },
   },
-  plugins: [],
+  plugins: [require('@dracor.org/react/tailwind')],
 };
