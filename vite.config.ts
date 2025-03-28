66e3e3e71879c66926c27366fdf319d5de68ae8f
Carsten Milling <cmil@hashtable.de>
2025-03-28 08:37:34 +0100

    Set up proxy for development server

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  // TODO: Make sure PROXY_PATH is configured without trailing '/' everywhere
+  const proxyPath = env.PROXY_PATH || '/exist/restxq/ecocor';
+
+  return {
+    base: '/',
+    plugins: [react()],
+    build: {
+      outDir: 'build',
+    },
+    server: {
+      open: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8090/',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, proxyPath),
+        },
+      },
+    },
+  };
+});
