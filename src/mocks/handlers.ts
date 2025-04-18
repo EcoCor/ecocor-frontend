90a07ba35119dc18df8910b3675d9adad5adc482
Carsten Milling <cmil@hashtable.de>
2025-04-18 18:41:16 +0200

    Upgrade msw

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,48 +1,58 @@
-import { rest } from 'msw';
+import { http, delay, HttpResponse } from 'msw';
 import { CorpusData, Text } from '../types';
 
 const corpora: CorpusData[] = require('./data/corpora.json');
 const textsMap: { [index: string]: Text[] } = require('./data/texts.json');
 
 const apiBase = import.meta.env.VITE_ECOCOR_API;
-const delay = parseInt(import.meta.env.VITE_MOCK_API_DELAY || '0');
+const d = parseInt(import.meta.env.VITE_MOCK_API_DELAY || '0');
 
 export const handlers = [
-  rest.get(`${apiBase}/corpora`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.delay(delay), ctx.json(corpora));
+  http.get(`${apiBase}/corpora`, async () => {
+    return HttpResponse.json(corpora, { status: 200 });
+    await delay(d);
   }),
 
-  rest.get(`${apiBase}/corpora/:corpusId`, (req, res, ctx) => {
-    const { corpusId } = req.params;
+  http.get(`${apiBase}/corpora/:corpusId`, async ({ params }) => {
+    const { corpusId } = params;
+    await delay(d);
     const corpus = corpora.find((c) => c.name === corpusId);
     if (corpus) {
-      return res(ctx.status(200), ctx.delay(delay), ctx.json(corpus));
+      return HttpResponse.json(corpus, { status: 200 });
     } else {
-      return res(ctx.status(404), ctx.json({ message: 'no such corpus' }));
+      return HttpResponse.json({ message: 'no such corpus' }, { status: 404 });
     }
   }),
 
-  rest.get(`${apiBase}/corpora/:corpusId/texts`, (req, res, ctx) => {
-    const { corpusId } = req.params;
+  http.get(`${apiBase}/corpora/:corpusId/texts`, async ({ params }) => {
+    const { corpusId } = params;
+    await delay(d);
     const texts = textsMap[corpusId as string];
     if (texts) {
-      return res(ctx.status(200), ctx.delay(delay), ctx.json(texts));
+      return HttpResponse.json(texts, { status: 200 });
     } else {
-      return res(ctx.status(404), ctx.json({ message: 'no such corpus' }));
+      return HttpResponse.json({ message: 'no such corpus' }, { status: 404 });
     }
   }),
 
-  rest.get(`${apiBase}/corpora/:corpusId/texts/:textName`, (req, res, ctx) => {
-    const { corpusId, textName } = req.params;
-    const texts = textsMap[corpusId as string];
-    if (texts) {
-      const text = texts.find((w) => w.name === textName);
-      if (text) {
-        return res(ctx.status(200), ctx.delay(delay), ctx.json(text));
+  http.get(
+    `${apiBase}/corpora/:corpusId/texts/:textName`,
+    async ({ params }) => {
+      const { corpusId, textName } = params;
+      await delay(d);
+      const texts = textsMap[corpusId as string];
+      if (texts) {
+        const text = texts.find((w) => w.name === textName);
+        if (text) {
+          return HttpResponse.json(text, { status: 200 });
+        }
+        return HttpResponse.json({ message: 'no such text' }, { status: 404 });
+      } else {
+        return HttpResponse.json(
+          { message: 'no such corpus' },
+          { status: 404 }
+        );
       }
-      return res(ctx.status(404), ctx.json({ message: 'no such text' }));
-    } else {
-      return res(ctx.status(404), ctx.json({ message: 'no such corpus' }));
     }
-  }),
+  ),
 ];
