0489c63881d0e6298648826a928ba9853729f00c
Carsten Milling <cmil@hashtable.de>
2025-04-21 23:55:58 +0200

    Set up vite tailwind plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -11,7 +12,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: '/',
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     build: {
       outDir: 'build',
     },
