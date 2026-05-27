0708f9b50a260c05a0014257eec1804c4121e7e1
Carsten Milling <carsten.milling@gmail.com>
2026-05-27 19:16:49 +0200

    Rename deprecated TanStackRouterVite export

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
   return {
     base: '/',
     plugins: [
-      TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+      tanstackRouter({ target: 'react', autoCodeSplitting: true }),
       react(),
       tailwindcss(),
     ],
